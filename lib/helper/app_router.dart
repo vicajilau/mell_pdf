2eb994bb5e15e8f203193b726839bf619ac21dea
Victor Carreras <victor.carreras.external@axa.com>
2022-07-19 17:43:10 +0200

    App routing added and some stuffs more

---
+++ lib/helper/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:mell_pdf/view/desktop/home_screen_desktop.dart';
+import 'package:platform_detail/platform_detail.dart';
+
+import '../view/desktop/splash_screen_desktop.dart';
+import '../view/mobile/home_screen_mobile.dart';
+import '../view/mobile/splash_screen_mobile.dart';
+
+class AppRouter {
+  late final Map<String, WidgetBuilder> routes;
+
+  AppRouter.init() {
+    if (PlatformDetails.isMobile) {
+      routes = getMobileRoutes();
+    } else {
+      routes = getDesktopRoutes();
+    }
+  }
+
+  // Mobile Routing
+  Map<String, WidgetBuilder> getMobileRoutes() {
+    return {
+      "/": (context) => const SplashScreenMobile(),
+      "/home": (context) => const HomeScreenMobile(),
+    };
+  }
+
+  // Desktop Routing + WEB
+  Map<String, WidgetBuilder> getDesktopRoutes() {
+    return {
+      "/": (context) => const SplashScreenDesktop(),
+      "/home": (context) => const HomeScreenDesktop(),
+    };
+  }
+}
