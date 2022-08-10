c425c946b528064bee7d267ea8ecf5d6048c2582
Victor Carreras <victor.carreras.external@axa.com>
2022-08-10 10:16:41 +0200

    file dialog warning removed

--- lib/helper/dialogs/file_dialog.dart
+++ lib/helper/dialogs/file_dialog.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mell_pdf/common/colors/colors_app.dart';
 import 'package:mell_pdf/common/localization/localization.dart';
 
-import '../helpers.dart';
-
 class FileDialog {
   static void add(
       {required BuildContext context,
@@ -12,9 +10,9 @@ class FileDialog {
     showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
-        title: Text(Localization.of(context).string('choose_an_option')), // Choose an option
-        content: Text(
-            Localization.of(context).string('content_file_dialog')),
+        title: Text(Localization.of(context)
+            .string('choose_an_option')), // Choose an option
+        content: Text(Localization.of(context).string('content_file_dialog')),
         // Do you want to load the image from your Gallery or file from File System?
         actions: <Widget>[
           TextButton(
