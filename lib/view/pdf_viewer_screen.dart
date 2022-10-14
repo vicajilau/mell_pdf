888e025dba0f00e86d054bfae31d81ebb06c76df
Victor Carreras <vicajilau@gmail.com>
2022-10-14 17:26:58 +0200

    Improved Share button catch bug

--- lib/view/pdf_viewer_screen.dart
+++ lib/view/pdf_viewer_screen.dart
@@ -4,6 +4,7 @@ import 'package:mell_pdf/model/models.dart';
 import 'package:pdfx/pdfx.dart';
 import 'package:share_plus/share_plus.dart';
 
+import '../helper/dialogs/custom_dialog.dart';
 import '../helper/utils.dart';
 
 class PDFViewerScreen extends StatelessWidget {
@@ -23,16 +24,26 @@ class PDFViewerScreen extends StatelessWidget {
             ? [
                 IconButton(
                     onPressed: () async {
-                      await Share.shareFiles(
-                        [file.getFile().path],
-                        text: Localization.of(context)
-                            .string('document_generated_with_drag_pdf'),
-                        sharePositionOrigin: Rect.fromLTRB(
-                            MediaQuery.of(context).size.width - 300,
-                            0,
-                            0,
-                            MediaQuery.of(context).size.height - 300),
-                      ); // Document Generated With Drag PDF
+                      try {
+                        await Share.shareFiles(
+                          [file.getFile().path],
+                          text: Localization.of(context)
+                              .string('document_generated_with_drag_pdf'),
+                          sharePositionOrigin: Rect.fromLTRB(
+                              MediaQuery.of(context).size.width - 300,
+                              0,
+                              0,
+                              MediaQuery.of(context).size.height - 300),
+                        ); // Document Generated With Drag PDF
+                      } catch (error) {
+                        CustomDialog.showError(
+                          context: context,
+                          error: error,
+                          titleLocalized: 'share_file_error_title',
+                          subtitleLocalized: 'share_file_error_subtitle',
+                          buttonTextLocalized: 'accept',
+                        );
+                      }
                     },
                     icon: const Icon(Icons.share))
               ]
