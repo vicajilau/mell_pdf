7d081de1f75468359968a6c40853e8e3c8cf79c8
ossyyrr <ossyyrr@hotmail.com>
2023-07-26 10:49:28 +0200

    Preview document screen - created

--- lib/view/pdf_viewer_screen.dart
+++ lib/view/pdf_viewer_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mell_pdf/common/localization/localization.dart';
 import 'package:mell_pdf/model/models.dart';
 import 'package:pdfx/pdfx.dart';
-import 'package:share_plus/share_plus.dart';
-
-import '../helper/dialogs/custom_dialog.dart';
-import '../helper/utils.dart';
 
 class PDFViewerScreen extends StatelessWidget {
   const PDFViewerScreen({Key? key}) : super(key: key);
@@ -20,78 +15,10 @@ class PDFViewerScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(file.getName()),
-        actions: isFinalFile(file)
-            ? [
-                // IconButton(
-                //   onPressed: () => showDialog<String>(
-                //     context: context,
-                //     builder: (BuildContext context) => AlertDialog(
-                //       title: Text(Localization.of(context)
-                //           .string('signature_title_alert')),
-                //       content: Text(Localization.of(context)
-                //           .string('signature_subtitle_alert')),
-                //       actions: [
-                //         TextButton(
-                //           onPressed: () {
-                //             Navigator.pop(context);
-                //           },
-                //           child: Text(Localization.of(context)
-                //               .string('signature_sign_alert')),
-                //         ),
-                //         TextButton(
-                //           onPressed: () async {
-                //             Navigator.pop(context, 'Scan');
-                //             Navigator.pushNamed(
-                //                 context, "/create_signature_screen",
-                //                 arguments: file);
-                //           },
-                //           child: Text(Localization.of(context)
-                //               .string('signature_create_alert')),
-                //         ),
-                //         TextButton(
-                //           onPressed: () => Navigator.pop(context, 'Cancel'),
-                //           child: Text(
-                //             Localization.of(context).string('cancel'), // Cancel
-                //             style: const TextStyle(color: ColorsApp.kMainColor),
-                //           ),
-                //         )
-                //       ],
-                //     ),
-                //   ),
-                //   icon: const Icon(Icons.create),
-                // ),
-                IconButton(
-                    onPressed: () async {
-                      try {
-                        await Share.shareXFiles(
-                          [XFile(file.getFile().path)],
-                          text: Localization.of(context)
-                              .string('document_generated_with_drag_pdf'),
-                          sharePositionOrigin: Rect.fromLTRB(
-                              MediaQuery.of(context).size.width - 300,
-                              0,
-                              0,
-                              MediaQuery.of(context).size.height - 300),
-                        ); // Document Generated With Drag PDF
-                      } catch (error) {
-                        CustomDialog.showError(
-                          context: context,
-                          error: error,
-                          titleLocalized: 'share_file_error_title',
-                          subtitleLocalized: 'share_file_error_subtitle',
-                          buttonTextLocalized: 'accept',
-                        );
-                      }
-                    },
-                    icon: const Icon(Icons.share))
-              ]
-            : null,
       ),
       body: PdfViewPinch(
         controller: pdfPinchController,
       ),
     );
   }
-
-  bool isFinalFile(FileRead file) => file.getName() == Utils.nameOfFinalFile;
 }
