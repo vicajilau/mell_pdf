78d548b4b4933517cd005a47d8c422efe02c04c2
Victor Carreras <victor.carreras-jimenez@capgemini.com>
2023-07-28 09:24:00 +0200

    Preview Document Fixed

--- lib/view/preview_document_screen.dart
+++ lib/view/preview_document_screen.dart
@@ -7,9 +7,14 @@ import 'package:share_plus/share_plus.dart';
 
 import '../helper/dialogs/custom_dialog.dart';
 
-class PreviewDocumentScreen extends StatelessWidget {
-  const PreviewDocumentScreen({Key? key}) : super(key: key);
+class PreviewDocumentScreen extends StatefulWidget {
+  const PreviewDocumentScreen({super.key});
 
+  @override
+  State<PreviewDocumentScreen> createState() => _PreviewDocumentScreenState();
+}
+
+class _PreviewDocumentScreenState extends State<PreviewDocumentScreen> {
   @override
   Widget build(BuildContext context) {
     final file = ModalRoute.of(context)!.settings.arguments as FileRead;
@@ -19,45 +24,45 @@ class PreviewDocumentScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(file.getName()),
+        title: const Text("DRAG PDF"),
         actions: [
-          IconButton(
-            onPressed: () => showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                title: Text(
-                    Localization.of(context).string('signature_title_alert')),
-                content: Text(Localization.of(context)
-                    .string('signature_subtitle_alert')),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context); // TODO
-                    },
-                    child: Text(Localization.of(context)
-                        .string('signature_sign_alert')),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      Navigator.pop(context, 'Scan');
-                      Navigator.pushNamed(context, "/create_signature_screen",
-                          arguments: file);
-                    },
-                    child: Text(Localization.of(context)
-                        .string('signature_create_alert')),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: Text(
-                      Localization.of(context).string('cancel'),
-                      style: const TextStyle(color: ColorsApp.kMainColor),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            icon: const Icon(Icons.create),
-          ),
+          // IconButton(
+          //   onPressed: () => showDialog<String>(
+          //     context: context,
+          //     builder: (BuildContext context) => AlertDialog(
+          //       title: Text(
+          //           Localization.of(context).string('signature_title_alert')),
+          //       content: Text(Localization.of(context)
+          //           .string('signature_subtitle_alert')),
+          //       actions: [
+          //         TextButton(
+          //           onPressed: () {
+          //             Navigator.pop(context); // TODO
+          //           },
+          //           child: Text(Localization.of(context)
+          //               .string('signature_sign_alert')),
+          //         ),
+          //         TextButton(
+          //           onPressed: () async {
+          //             Navigator.pop(context, 'Scan');
+          //             Navigator.pushNamed(context, "/create_signature_screen",
+          //                 arguments: file);
+          //           },
+          //           child: Text(Localization.of(context)
+          //               .string('signature_create_alert')),
+          //         ),
+          //         TextButton(
+          //           onPressed: () => Navigator.pop(context, 'Cancel'),
+          //           child: Text(
+          //             Localization.of(context).string('cancel'),
+          //             style: const TextStyle(color: ColorsApp.kMainColor),
+          //           ),
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //   icon: const Icon(Icons.create),
+          // ),
           IconButton(
               onPressed: () async {
                 try {
